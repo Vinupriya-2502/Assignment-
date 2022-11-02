dba5c3e4d96da56c6828aa90e266425026450c52
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2022-11-02 08:35:58 -0700

    Create swapping_tb.v

---
+++ 11) swapping/swapping_tb.v
@@ -0,0 +1,13 @@
+module swapping_tb;
+reg [15:0]a;
+wire [15:0]b;
+swapping dut(a,b);
+initial 
+begin
+#2 a=15'b010100001101010;
+#4 a=15'b010101010101000;
+#10 $finish;
+end
+initial 
+$monitor($time,"a=%b\t,outputvalueafter swappingb=%b\t",a,b);
+endmodule
